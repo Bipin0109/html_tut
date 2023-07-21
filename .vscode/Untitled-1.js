01f34e8ce1151ea3c0d43fd750ba187c543b500c
bipin0109 <bpbihari192002@gmail.com>
2023-07-21 10:46:49 +0530

    first commit

---
+++ .vscode/Untitled-1.js
@@ -0,0 +1,69 @@
+
+document.body.innerHTML='hello';
+'hello'
+document.body.innerHTML='nikal-laude';
+'nikal-laude'
+document.body.innerHTML='...';
+'...'
+54/8
+6.75
+326/8
+40.75
+1/242.75
+0.004119464469618949
+10.90*2+20.95
+42.75
+1+1*3
+4
+56+(6+54)*2
+176
+.1+.2
+0.30000000000000004
+Celsius =25 ;f=(Celsius*9/5)+32;console.log(f);
+
+'some'+'time'
+'sometime'
+
+'lata'+'mangeshkar'
+'latamangeshkar'
+
+typeof 2
+'number'
+
+typeof 'amit'
+'string'
+
+'gge'+9
+'gge9'
+console.log('hello');
+hello
+
+'$'+56+56
+'$5656'
+'$'+(87+5+8)
+'$100'
+"$34"+(45+54)
+'$3499'
+'$'+(34+45+54)
+'$133'
+'Items ('+(1+1)+'):$'+(45+544)
+'Items (2):$589'
+
+"I\'am learning javascript"
+"I'am learning javascript"
+
+'I\'  am bipin'
+"I'  am bipin"
+
+`bipin(6)`
+'bipin(6)'
+
+`total_cost({979+11})`
+'total_cost({979+11})'
+
+`total_cost(${979+11})`
+'total_cost(990)'
+
+`lata
+mangeskar`
+'lata\nmangeskar'
